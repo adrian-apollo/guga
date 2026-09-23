20d64e361acb20e32b1827c81cc99c8ec64c2227
adrian-apollo <alfapollodesign@gmail.com>
2026-09-23 08:29:22 -0400

    game update and other stuff

--- addons/guga/Premade components/c_projectile_linear_movement.gd
+++ addons/guga/Premade components/c_projectile_linear_movement.gd
@@ -1,11 +1,12 @@
 extends ComponentBase
 class_name CPorjectileLinearMovement
 
+@export var direction:Vector2 = Vector2(0,0)
+
 var ownerref:Node2D
 
 func begin( ):
 	ownerref = owner
 
 func tick() -> void:
-	ownerref.position.x += 5
-	
+	ownerref.position += direction
